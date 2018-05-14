cfa9b194048bad25262c0249d07dd8b8de01dd55
Timon de Groot <tdegroot96@gmail.com>
2018-05-14 13:40:22 +0200

    Fix nginx optimal types hash

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,8 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+# Documentation:
+# - https://testinfra.readthedocs.io/en/latest/modules.html#host
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
@@ -19,6 +21,6 @@ def test_nginx_is_installed(host):
     assert nginx.is_installed
 
 
-def test_nginx_has_optimale_types_hash(host):
-    output = host.run('nginx -t').stdout
-    assert '[warn] could not build optimal types_hash' not in output
+def test_nginx_has_optimal_types_hash(host):
+    nginx = host.run('nginx -t')
+    assert '[warn] could not build optimal types_hash' not in nginx.stderr
